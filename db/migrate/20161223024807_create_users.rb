828e9b00b76b8014124f7b9feb041a25ac402cd2
user <likhimsiow@yahoo.com>
2016-12-23 11:49:45 +0800

    Added name, gender, phone_num fields to Users table

---
+++ db/migrate/20161223024807_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.timestamps null: false
+      t.string :name
+      t.string :email, null: false
+      t.string :encrypted_password, limit: 128, null: false
+      t.string :confirmation_token, limit: 128
+      t.string :remember_token, limit: 128, null: false
+      t.string :gender
+      t.integer :phone_num
+      t.date :dob
+    end
+
+    add_index :users, :email
+    add_index :users, :remember_token
+  end
+end
