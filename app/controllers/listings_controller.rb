39324b2ffe3dc6ef2bd879ff0ebe0af075447e62
user <likhimsiow@yahoo.com>
2016-12-27 18:15:55 +0800

    Added listing controller & view

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,11 +1,21 @@
 class ListingsController < ApplicationController
 
 	def index
+		@listings = Listing.all
 	end
 
 	def new
+		@listing = Listing.new
 	end
 
 	def create
+		@listing = Listing.new(listing_params)
+		if @listing.save
+			redirect_to listings_path
+		end
+	end
+
+	def listing_params
+		params.require(:listing).permit(:title, :description, :max_guests, :price)
 	end
 end
