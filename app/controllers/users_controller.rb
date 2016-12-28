0d2498d28e7503a55b5e39c6a0ca917df82034d3
user <likhimsiow@yahoo.com>
2016-12-29 01:39:34 +0800

    Create user page: 1. Redo UsersController to use from ApplicationController instead of Clearance 2. Changed root to users/index 3. Removed code from new.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,55 @@
-# Inherit from source code Clearance's Users' Controller
-class UsersController < Clearance::UsersController
+# # Inherit from source code Clearance's Users' Controller
+# class UsersController < Clearance::UsersController
 
-	# Want to redefine user_from_params 
-	# Hence override default clearance's user's controller
-	def create
-		@user = User.new(user_from_params)
-		if @user.save
-			sign_in @user
-			redirect_back_or url_after_create
+# 	# Want to redefine user_from_params 
+# 	# Hence override default clearance's user's controller
+# 	def create
+# 		@user = User.new(user_from_params)
+# 		if @user.save
+# 			sign_in @user
+# 			redirect_back_or url_after_create
+# 		else
+# 			render template: "users/new"
+# 		end
+# 	end
+
+# 	private #to not make user_from_params a method
+
+# 	def user_from_params
+# 		params.require(:user).permit(:name, :email, :password, :gender, :phone_num, :dob)
+# 	end
+# end
+
+class UsersController < ApplicationController
+	#DRY:find_user params
+	before_action :find_user, only: [:show, :edit, :update]
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+		@reservations = @user.reservations
+	end
+
+	def edit
+	end
+
+	def update
+		if @user.update(user_params)
+			flash[:success] = "Update profile successful"
+			redirect_to @user #user_path
 		else
-			render template: "users/new"
+			flash[:danger] = "Error: Updating user incomplete"
+			render :edit
 		end
 	end
 
-	private #to not make user_from_params a method
-
-	def user_from_params
-		params.require(:user).permit(:name, :email, :password, :gender, :phone_num, :dob)
+	def find_user
+		@user = User.find(params[:id])
 	end
 
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :gender, :phone_num, :dob)
+	end
 end
