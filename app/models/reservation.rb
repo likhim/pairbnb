354f5c256784ec8bed71896c5d3f8c7b63dba69d
user <likhimsiow@yahoo.com>
2016-12-28 18:36:21 +0800

    Included followings for reservation: 1. Updated model with validations on overlapping dates & max guests 2. Included error msgs in model, controller & view(in _form)

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,28 @@
 class Reservation < ActiveRecord::Base
   belongs_to :user
   belongs_to :listing
+
+  validate :check_overlapping_dates
+  validate :check_max_guests
+
+  def check_overlapping_dates
+  	#check if new bookings overlapped with any existing bookings 
+  	listing.reservations.each do |older_reservation|
+  		if overlap?(self, older_reservation)
+  			return errors.add(:overlapping_dates, "Dates choosen are not available")
+  		end
+  	end
+  end
+
+  def overlap?(x,y)
+  	(x.checkin_date - y.checkout_date) * (y.checkin_date - x.checkout_date) > 0
+  end
+
+  def check_max_guests
+  	max_guests = listing.max_guests
+  	if num_guests > max_guests
+  	errors.add(:max_guests, "Exceeded number of max guest allowed")
+  	end
+  end
+
 end
