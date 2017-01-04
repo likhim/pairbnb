e47fb3c68c95b42492709fbadac9e4bf4c233752
user <likhimsiow@yahoo.com>
2017-01-04 12:33:15 +0800

    Mailer using AvtiveJob method

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -13,8 +13,7 @@ before_action :find_reservation, only: [:show, :edit, :update]
 		@reservation.listing = @listing
 		if @reservation.save
 			# invoke Job by calling the method .perform then _later is to perform once queue is free
-			# ReservationJob.perform_later(@reservation.user, @listing.user, @reservation.id)
-			ReservationMailer.booking_email(@reservation.user, @listing.user, @reservation.id).deliver_now
+			ReservationJob.perform_later(@reservation.user, @listing.user, @reservation.id)
 			# ReservationMailer will be invoke in Job - To send mailer to host once booking is saved
 			redirect_to current_user #or redirect_to @listings
 		else
